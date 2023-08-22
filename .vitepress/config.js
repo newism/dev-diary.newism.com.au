f9364e0974f189964dce54d2f46df51cfcf3d1d5
Leevi Graham <leevi@newism.com.au>
2023-08-23 08:16:13 +1000

    Added analytics

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,6 +4,18 @@ export default defineConfig({
   title: 'Newism Dev Diary',
   description: 'CraftCMS development notes, tips and tricks.',
   head: [
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-FX9N6QZ21K',
+      },
+    ],
+    [
+      'script',
+      {},
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-FX9N6QZ21K');",
+    ],
     ['meta', { name: 'twitter:site', content: '@newism' }],
     ['meta', { name: 'twitter:card', content: 'summary' }],
     [
